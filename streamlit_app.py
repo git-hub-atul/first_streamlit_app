f7d1dbaeca8ed322b27bc3ec9e217ef57cf14d5a
git-hub-atul <142893269+git-hub-atul@users.noreply.github.com>
2023-08-22 16:35:15 +0530

    Update streamlit_app.py
    Added multiple texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and BlueBerry Oat Meal')
+streamlit.text('Kale Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
